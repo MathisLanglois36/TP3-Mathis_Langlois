cebfeec0bccd48409b5df94c4226c4456789c961
OlivierLandry50 <mathis.langlois54@hotmail.com>
2026-05-08 14:30:02 -0400

    ajouteter constante de message et condition pour faire le set nom et prix

--- src/logique/Ingredient.java
+++ src/logique/Ingredient.java
@@ -2,6 +2,12 @@ package logique;
 
 public class Ingredient
 {
+    public static final int MIN_CARACTERS_NOM=6;
+    public static final int PRIX_INVALIDE=0;
+    public static final String MESSAGE_VALEUR_NULL_NON_PERMISE = "Valeur null non permise";
+    public static final String MESSAGE_NOM_TROP_COURT = "Nom trop court";
+    public static final String MESSAGE_PRIX_INVALIDE = "Prix invalide";
+
     private String nom;
     private int prix;
 
@@ -16,6 +22,11 @@ public class Ingredient
     }
 
     private void setNom(String nom) {
+        if (nom == null){
+            throw new IllegalArgumentException(MESSAGE_VALEUR_NULL_NON_PERMISE);
+        }else if (nom.length() < MIN_CARACTERS_NOM){
+            throw new IllegalArgumentException(MESSAGE_NOM_TROP_COURT);
+        }
         this.nom = nom;
     }
 
@@ -24,6 +35,9 @@ public class Ingredient
     }
 
     private void setPrix(int prix) {
+        if (prix < PRIX_INVALIDE){
+            throw new IllegalArgumentException(MESSAGE_PRIX_INVALIDE);
+        }
         this.prix = prix;
     }
 
